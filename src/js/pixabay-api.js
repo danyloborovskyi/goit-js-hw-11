0a46447c85ba689c4928d0402686e4f6158f2ca7
Danylo Borovskyi <danilborovskii199911@gmail.com>
2025-06-14 17:53:38 +0300

    function markup is created

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -7,11 +7,12 @@ const API_KEY = "50839747-8e280c8a680bf22042cf2c364"
 
 const form = document.querySelector(".form")
 const input = document.querySelector('[name="search-text"]')
+const list = document.querySelector(".gallery")
 
-function getImagesByQuery() {
+function getImagesByQuery(query) {
     const params = {
     key: API_KEY,
-    q: input.value,
+    q: query,
     image_type: "photo",
     orientation: "horizontal",
     safesearch: true
@@ -19,8 +20,8 @@ function getImagesByQuery() {
     axios("https://pixabay.com/api/", {params})
     .then(res => {
         if (res.data.hits.length > 0) {
-            console.log(res)
-            console.log("Params:", params);
+            console.log(res);
+            list.innerHTML = createMarkup(res.data.hits)
         } else {
             iziToast.error({
                 message: 'Sorry, there are no images matching your search query. Please try again!',
@@ -35,8 +36,34 @@ form.addEventListener("submit", handleSubmit)
 
 function handleSubmit(event) {
     event.preventDefault()
+    const query = input.value.trim();
+    getImagesByQuery(query)
+}
 
-    getImagesByQuery()
+function createMarkup(array) {
+    return array.map(({ webformatURL, largeImageURL, tags, likes, views, comments, downloads}) => `
+        <li>
+            <img src="${webformatURL}" width="300" data-largeimg="${largeImageURL}" alt="${tags}">
+            <ul>
+                <li>
+                    <h2>Likes</h2>
+                    <p>${likes}</p>
+                </li>
+                <li>
+                    <h2>Views</h2>
+                    <p>${views}</p>
+                </li>
+                <li>
+                    <h2>Comments</h2>
+                    <p>${comments}</p>
+                </li>
+                <li>
+                    <h2>Downloads</h2>
+                    <p>${downloads}</p>
+                </li>
+            </ul>
+        </li>
+    `).join("")
 }
 
 
