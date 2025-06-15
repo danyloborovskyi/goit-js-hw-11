15ed1b2db9723a67bfcbf20c615c92c4b61b84d9
Danylo Borovskyi <danilborovskii199911@gmail.com>
2025-06-15 17:28:58 +0300

    done

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,15 +1,11 @@
 import axios from 'axios';
 
-import iziToast from "izitoast";
+
 import "izitoast/dist/css/iziToast.min.css";
 
 const API_KEY = "50839747-8e280c8a680bf22042cf2c364"
 
-const form = document.querySelector(".form")
-const input = document.querySelector('[name="search-text"]')
-const list = document.querySelector(".gallery")
-
-function getImagesByQuery(query) {
+export function getImagesByQuery(query) {
     const params = {
     key: API_KEY,
     q: query,
@@ -17,55 +13,7 @@ function getImagesByQuery(query) {
     orientation: "horizontal",
     safesearch: true
 }
-    axios("https://pixabay.com/api/", {params})
-    .then(res => {
-        if (res.data.hits.length > 0) {
-            console.log(res);
-            list.innerHTML = createMarkup(res.data.hits)
-        } else {
-            iziToast.error({
-                message: 'Sorry, there are no images matching your search query. Please try again!',
-            });
-        }
-        })
-    .catch(err => console.log(err)
-    );
+     return axios.get("https://pixabay.com/api/", {params}).then(res => res.data)
 }
-
-form.addEventListener("submit", handleSubmit)
-
-function handleSubmit(event) {
-    event.preventDefault()
-    const query = input.value.trim();
-    getImagesByQuery(query)
-}
-
-function createMarkup(array) {
-    return array.map(({ webformatURL, largeImageURL, tags, likes, views, comments, downloads}) => `
-        <li>
-            <img src="${webformatURL}" width="300" data-largeimg="${largeImageURL}" alt="${tags}">
-            <ul>
-                <li>
-                    <h2>Likes</h2>
-                    <p>${likes}</p>
-                </li>
-                <li>
-                    <h2>Views</h2>
-                    <p>${views}</p>
-                </li>
-                <li>
-                    <h2>Comments</h2>
-                    <p>${comments}</p>
-                </li>
-                <li>
-                    <h2>Downloads</h2>
-                    <p>${downloads}</p>
-                </li>
-            </ul>
-        </li>
-    `).join("")
-}
-
-
 
 
--- src/main.js
+++ src/main.js
@@ -1,1 +1,42 @@
-import foolog from './js/pixabay-api'
+import iziToast from "izitoast";
+
+import { getImagesByQuery } from "./js/pixabay-api"
+import { createGallery, clearGallery, refreshLightbox, showLoader, hideLoader } from "./js/render-functions";
+
+const form = document.querySelector(".form")
+const input = document.querySelector('[name="search-text"]')
+const list = document.querySelector(".gallery")
+
+form.addEventListener("submit", handleSubmit)
+
+function handleSubmit(event) {
+    event.preventDefault()
+    const query = input.value.trim();
+
+    if (!query) {
+        return;
+    }
+
+    clearGallery(list);
+
+    showLoader()
+    
+    getImagesByQuery(query).then(data => {
+        if (data.hits.length > 0) {
+            console.log(data);
+            list.innerHTML = createGallery(data.hits)
+            refreshLightbox();
+        } else {
+            iziToast.error({
+                message: 'Sorry, there are no images matching your search query. Please try again!',
+            });
+        }
+    })
+    .catch(error => console.log(error))
+    .finally(() => {
+        hideLoader();
+        input.value = "";
+    })
+}
+
+refreshLightbox();
